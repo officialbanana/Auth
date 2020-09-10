2e6a478095a8445c12afb6252556cad063e6ab32
officialbanana <62805568+officialbanana@users.noreply.github.com>
2020-09-10 12:47:55 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,4 @@
+function setup() {
+createCanvas(400,400)
+text("Hello, AuthorizedUser, to enter please, enter your name at this page, https://docs.google.com/document/d/18oo4zMH07lknzSEzg9iAiSJaWNn6Dyg0HgKosJhtoP8/edit",150,200,150,150);
+}
